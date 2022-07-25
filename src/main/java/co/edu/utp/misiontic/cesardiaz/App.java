7b9473d4733990c0f80e24481f961780e7526c3d
Cesar Augusto Diaz Arriaga <black@utp.edu.co>
2022-07-25 18:31:34 -0500

    Pruebas de pago de la mesa

--- src/main/java/co/edu/utp/misiontic/cesardiaz/App.java
+++ src/main/java/co/edu/utp/misiontic/cesardiaz/App.java
@@ -1,5 +1,6 @@
 package co.edu.utp.misiontic.cesardiaz;
 
+import co.edu.utp.misiontic.cesardiaz.excepcion.PagoInsuficienteException;
 import co.edu.utp.misiontic.cesardiaz.modelo.Adicional;
 import co.edu.utp.misiontic.cesardiaz.modelo.Bandeja;
 import co.edu.utp.misiontic.cesardiaz.modelo.Carne;
@@ -20,6 +21,7 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
@@ -35,7 +37,7 @@ public final class App {
         var opcion1 = new Completo(precio, sopa, principio, carne, ensalada, jugo);
 
         var pedido = new Pedido("Cesar Diaz", opcion1);
-        //pedido.agregarAdicional(new Adicional("Chicharron", 3_000));
+        pedido.agregarAdicional(new Adicional("Chicharron", 3_000));
         mesa.agregarPedido(pedido);
 
         precio = 10_000;
@@ -44,5 +46,18 @@ public final class App {
         mesa.agregarPedido(pedido);
 
         System.out.printf("Total de la mesa: $ %,d. %n", mesa.calcularTotal());
+
+        var efectivo = 25_000;
+        try {
+            var devuelta = mesa.pago(efectivo);
+            System.out.printf("Paga la cuenta con $ %,d y recibe una devuelta de $ %,d %n",
+                    efectivo, devuelta);
+        } catch (PagoInsuficienteException ex) {
+            System.err.printf("Creo que va a tener que estregar algunos platos!!. Pagó solo $ %,d. %n", efectivo);
+        }
+
+        System.out.printf("Ahora el total de la mesa: $ %,d. %n", mesa.calcularTotal());
+
+
     }
 }
