3c01c49cac0dacc676d3a1def14e2f33f456e3c1
Cesar Augusto Diaz Arriaga <black@utp.edu.co>
2022-07-29 17:53:05 -0500

    Agregando datos al menu

--- src/main/java/co/edu/utp/misiontic/cesardiaz/App.java
+++ src/main/java/co/edu/utp/misiontic/cesardiaz/App.java
@@ -3,6 +3,8 @@ package co.edu.utp.misiontic.cesardiaz;
 import java.util.Scanner;
 
 import co.edu.utp.misiontic.cesardiaz.controlador.RestauranteController;
+import co.edu.utp.misiontic.cesardiaz.excepcion.ObjetoNoExistenteException;
+import co.edu.utp.misiontic.cesardiaz.excepcion.PagoInsuficienteException;
 
 /**
  * Hello world!
@@ -21,26 +23,29 @@ public final class App {
         try (var sc = new Scanner(System.in)) {
 
             var controlador = new RestauranteController(sc);
-
             controlador.iniciarBaseDatos();
 
+            controlador.iniciarAplicacion();
 
-            var mesa = controlador.consultarMesa("01");
-            System.out.println("La mesa consultada es: " + mesa);
 
-            controlador.agregarPedido(mesa);
-            System.out.println("El total de la mesa es: " + mesa.calcularTotal());
-
-            var devuelta = controlador.pagarCuenta(mesa);
-            System.out.printf("Los clientes de la mesa %s pagaron la cuenta y se devolvió $ %,d %n",
-                mesa, devuelta);
-
-            // pruebaVenta();
         } catch (Exception ex) {
             System.err.println("Ocurrio un error y salgo de la aplicación: \n\t" + ex.getMessage());
             ex.printStackTrace();
         }
 
+    }
+
+    private static void prueba2(RestauranteController controlador)
+            throws ObjetoNoExistenteException, PagoInsuficienteException {
+        var mesa = controlador.consultarMesa("01");
+        System.out.println("La mesa consultada es: " + mesa);
+
+        controlador.agregarPedido(mesa);
+        System.out.println("El total de la mesa es: " + mesa.calcularTotal());
+
+        var devuelta = controlador.pagarCuenta(mesa);
+        System.out.printf("Los clientes de la mesa %s pagaron la cuenta y se devolvió $ %,d %n",
+            mesa, devuelta);
     }
 
 }
--- src/main/java/co/edu/utp/misiontic/cesardiaz/vista/PrincipalView.java
+++ src/main/java/co/edu/utp/misiontic/cesardiaz/vista/PrincipalView.java
@@ -17,22 +17,64 @@ public class PrincipalView {
     public void iniciarAplicacion() {
         var mostrarMenu = true;
         while (mostrarMenu) {
-            mostrarMenu();
+            System.out.println(".: RESTAURANTE EL CORRIENTAZO :.");
+            System.out.println("1 -> Gestion de pedidos");
+            System.out.println("2 -> Gestion de datos maestros");
+            System.out.println("0 -> Salir de la aplicación");
+
             var opcion = leerEntero("Ingrese su opcion: ");
             switch (opcion) {
-                case 1: // Agregar Mesas
-                    
+                case 0:
+                    mostrarMenu = false;
+                    System.out.println("Hasta pronto!");
                     break;
-            
+                case 1:
+                    gestionPedidos();
+                    break;
+                case 2:
+                    gestionDatosMaestros();
+                    break;
                 default:
+                    System.out.println("Opcion inválida. Intente de nuevo");
                     break;
             }
 
         }
     }
 
-    private void mostrarMenu() {
+    private void gestionDatosMaestros() {
+        System.out.println(".: GESTION DE DATOS MAESTROS :. ");
+    }
 
+    private void gestionPedidos() {
+        var mesa = controller.seleccionarMesa();
+        
+        System.out.println(".: GESTION DE PEDIDOS :.");
+        System.out.println("1 -> Agregar pedido a mesa");
+        System.out.println("2 -> Agregar adicional a mesa");
+        System.out.println("3 -> Pagar deuda mesa");
+        System.out.println("4 -> Consultar estado de la mesa");
+        System.out.println("0 -> Volver al menu principal");
+        var opcion = leerEntero("Ingrese su opcion: ");
+        switch (opcion) {
+            case 0:
+                esperarEnter();
+                break;
+            case 1:
+                
+                break;
+            case 2:
+                gestionDatosMaestros();
+                break;
+            default:
+                System.out.println("Opcion inválida. Intente de nuevo");
+                break;
+        }
+    }
+
+    private void esperarEnter() {
+        System.out.print("Presione Enter para continuar.");
+        entrada.nextLine();
     }
 
     private Integer leerEntero(String mensaje) {
@@ -40,12 +82,12 @@ public class PrincipalView {
     }
 
     private Integer leerEntero(String mensaje, Integer minValue, Integer maxValue) {
-            Integer opcion = null;
+        Integer opcion = null;
         do {
             try {
                 System.out.print(mensaje);
                 opcion = entrada.nextInt();
-                if(opcion > maxValue) {
+                if (opcion > maxValue) {
                     System.err.println("Opcion inválida\n Intente de nuevo");
                     opcion = null;
                 }
@@ -54,7 +96,7 @@ public class PrincipalView {
             } finally {
                 entrada.nextLine();
             }
-        } while(opcion == null);
+        } while (opcion == null);
         return opcion;
     }
 }
